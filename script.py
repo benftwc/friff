f100eae065771774279613e53931db137625aacc
Benjamin <benftwc@users.noreply.github.com>
2022-11-14 21:59:58 +0100

    improve quite a lot main script

--- script.py
+++ script.py
@@ -1,23 +1,89 @@
 # Imports
+import argparse
+import random
+import string
+
 from PIL import Image, ImageChops
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import argparse
+
+import settings as SETTINGS
 
 # Prepare Arguments Parser
-parser = argparse.ArgumentParser(description="Take screenshot and compare them to get differences between two given environments")
-parser.add_argument('-s', '--source', metavar='URL',dest='source_url', type=str, help="The source website URL")
-parser.add_argument('-t', '--target', metavar='URL',dest='target_url', type=str, help="The target website URL")
-parser.add_argument('-k', '--keep-files', dest='keep_files', type=bool, default=True, help="True to keep files after comparison")
-parser.add_argument('-p', '--purge-files', dest='purge_files', type=bool,default=True, help='True to remove files before take screenshots')
-parser.add_argument('-o', '--open-result', dest='open_result', type=bool, default=False, help='Show result picture once it generated')
+parser = argparse.ArgumentParser(
+    description="Take screenshot and compare them to get differences between two given environments"
+)
+parser.add_argument(
+    "-s",
+    "--source",
+    metavar="URL",
+    dest="source_url",
+    help="The source website URL",
+)
+parser.add_argument(
+    "-t",
+    "--target",
+    metavar="URL",
+    dest="target_url",
+    help="The target website URL",
+)
+parser.add_argument(
+    "-k",
+    "--keep-files",
+    dest="keep_files",
+    default=False,
+    help="Keep files after comparison",
+    action='store_true',
+)
+parser.add_argument(
+    "-p",
+    "--purge-files",
+    dest="purge_files",
+    default=False,
+    help="Remove files before take screenshots",
+    action='store_true',
+)
+parser.add_argument(
+    "-o",
+    "--open-result",
+    dest="open_result",
+    default=False,
+    help="Show result picture once it generated",
+    action='store_true',
+)
+parser.add_argument(
+    "-w",
+    "--warmup-time",
+    dest="warmup_time",
+    type=int,
+    default=0,
+    help="Wait before starting Selenium once it's driver is ready (seconds)",
+)
+parser.add_argument(
+    "-v",
+    "--verbose",
+    dest="verbose",
+    default=False,
+    help="Add some debug texts",
+    action='store_true',
+)
 args = parser.parse_args()
 
-print(args)
-
 # Lib internals
+def trace_debug(message, enabled=args.verbose):
+    """Trace logs/message once verbose mode is enabled
+
+    Args:
+        message (string): Message to display
+        enabled (boolean, optional): Enable verbose mode. Defaults to True.
+    """
+    if enabled:
+        print(message)
+    
+trace_debug(args)
+
 def pixel_diffs(source, comparison):
     """Do the magic.
 
@@ -37,45 +103,91 @@ def pixel_diffs(source, comparison):
         Image: Render of source MINUS target (black output if no diff)
     """
     if source.size != comparison.size:
-        raise Exception("Different image sizes")
+        raise SETTINGS.ImageComparisonException("Different image sizes")
 
     if source.mode != comparison.mode:
-        raise Exception("Different picture mods")
+        raise SETTINGS.ImageComparisonException("Different picture mods")
 
     diff = ImageChops.difference(source, comparison)
 
-    return diff.convert('L')
+    return diff.convert("L")
 
-def generate_screenshot(website, type='source', extension='png'):
+def get_random_name(size=10):
+    """Generate random string
+
+    Args:
+        size (int, optional): String length. Defaults to 10.
+
+    Returns:
+        string: Random string
+    """
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(size))
+
+def get_driver(driver_name="chrome"):
+    """Prepare Webdriver to be used
+
+    Returns:
+        WebDriver: WebDriver instance warmed up
+    """
+    options = Options()
+    options.headless = True
+
+    driver_name = driver_name.lower()
+    driver = None
+    
+    if driver_name == "chrome":
+        driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=options
+        )
+    
+    if driver_name == "firefox":
+        driver = webdriver.Firefox(
+            service=Service(ChromeDriverManager().install()), options=options
+        )
+    
+    if driver is None:
+        raise SETTINGS.WebdriverVehicleError(f"Vehicle {driver} does not exists yet.")
+    
+    driver.set_window_position(0, 0)
+    
+    return driver
+    
+def generate_screenshot(website, name, width=SETTINGS.webdriver_width_default, height=SETTINGS.webdriver_height_default):
     """Based on its URL, generate Website screenshot
 
     Args:
         website (str): Website to screenshot
         type (str, optional): Output filename. Defaults to 'source'.
-        extension (str, optional): Output file extension. Defaults to 'png'.
+        width (int, optional): The webdriver width. Defaults to SETTINGS.webdriver_width_default (1920).
+        height (int, optional): The webdriver height. Defaults to SETTINGS.webdriver_height_default (1080).
+
+    Raises:
+        SETTINGS.WebdriverSizeError: Size does not respect min/max values
 
     Returns:
         str: The saved screenshot path
     """
-    options = Options()
-    options.headless = True
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver.set_window_position(0,0)
-    driver.set_window_size(1920,1080)
+    if int(width) < SETTINGS.webdriver_width_min or int(width) > SETTINGS.webdriver_width_max:
+        raise SETTINGS.WebdriverSizeError()
+    
+    driver = get_driver()
     driver.get(website)
-
-    screenshot_name = f"{type}.{extension}"
+    driver.set_window_size(width, height)
+    screenshot_name = f"{name}.png"
     driver.save_screenshot(screenshot_name)
     driver.quit()
 
     return screenshot_name
 
 
-
 ## RUNNER
+## TO REFACTOR :kekw:
 # Prepare to-compare files
-compare = [generate_screenshot(args.source_url, "source"), generate_screenshot(args.target_url, "target")]
+session = get_random_name()
+compare = [
+    generate_screenshot(website=args.source_url, name=f"s-{session}"),
+    generate_screenshot(website=args.target_url, name=f"t-{session}"),
+]
 
 # Sort file instances
 img_source = Image.open(f"{compare[0]}")
