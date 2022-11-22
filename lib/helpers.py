c987df0167e3fc77f10553f63543741dc42ef0ac
Benftw <benftwc@gmail.com>
2022-11-22 21:09:24 +0100

    Improve Random entropy and add helper method to create writable directories

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,3 +1,4 @@
+import os
 import random
 import string
 
@@ -11,4 +12,12 @@ def get_random_name(size=10):
     Returns:
         string: Random string
     """
-    return "".join(random.choice(string.ascii_lowercase) for i in range(size))
+    return "".join(
+        random.choice(string.ascii_lowercase + string.digits) for i in range(size)
+    )
+
+
+def create_writable_directory(dir_name):
+    umask = os.umask(0o000)
+    os.mkdir(dir_name, 0o766)
+    os.umask(umask)
