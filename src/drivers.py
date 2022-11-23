866458d5cbf00091ec5d5010c5a08427e6ca740c
Benjamin Cassinat <benjamin.cassinat@conserto.pro>
2022-11-23 14:16:20 +0100

    FIX Linters

--- src/drivers.py
+++ src/drivers.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.options import Options as GeckoOptions
+from selenium.webdriver.firefox.service import Service as GeckoService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 from settings import Debug, WebdriverVehicleError
 
@@ -15,28 +21,19 @@ def get_driver(driver_name="chrome"):
     driver = None
 
     if driver_name == "chrome":
-        from selenium.webdriver.chrome.options import Options
-        from selenium.webdriver.chrome.service import Service
-        from webdriver_manager.chrome import ChromeDriverManager
-
-        options = Options()
+        options = ChromeOptions()
         options.headless = True
-
         driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager("107.0.5304.62").install()),
+            service=ChromeService(ChromeDriverManager("107.0.5304.62").install()),
             options=options,
         )
 
     if driver_name == "firefox":
-        from selenium.webdriver.firefox.options import Options
-        from selenium.webdriver.firefox.service import Service
-        from webdriver_manager.firefox import GeckoDriverManager
-
-        options = Options()
+        options = GeckoOptions()
         options.headless = True
 
         driver = webdriver.Firefox(
-            service=Service(GeckoDriverManager().install()), options=options
+            service=GeckoService(GeckoDriverManager().install()), options=options
         )
 
     if driver is None:
