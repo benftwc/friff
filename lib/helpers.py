6696a944e3ecba55572d08f0dd82f645c24a2825
Benjamin Cassinat <benjamin.cassinat@conserto.pro>
2022-11-23 11:36:28 +0100

    Add Initialization method to ensure we got render folder in fresh installs

--- lib/helpers.py
+++ lib/helpers.py
@@ -18,6 +18,20 @@ def get_random_name(size=10):
 
 
 def create_writable_directory(dir_name):
+    """Create writable directory to prepare render/temporary saves folder
+
+    Args:
+        dir_name (string): Folder path, relative or absolute
+    """
     umask = os.umask(0o000)
     os.mkdir(dir_name, 0o766)
     os.umask(umask)
+
+def init_render_folder(dirname):
+    """Create the default render folder - Called on app init
+
+    Args:
+        dirname (string): Folder path, relative or absolute
+    """
+    if not os.path.exists(dirname):
+        create_writable_directory(dirname)
